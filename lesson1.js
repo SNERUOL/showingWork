2c4afc7efcf68ed7d8e0cd2e38f650da4c1e19e1
SNERUOL <153216108+SNERUOL@users.noreply.github.com>
2023-12-27 21:48:50 +0300

    lesson1.js

--- lesson1.js
+++ lesson1.js
@@ -1,1 +1,41 @@
-
+const valueWillNotChange = 20;
+undefined
+valueWillNotChange = 30;
+Uncaught TypeError: invalid assignment to const 'valueWillNotChange'
+    <anonymous> debugger eval code:1
+debugger eval code:1:1
+let valueWillChange = 20;
+undefined
+let valueWillChange = 30;
+Uncaught SyntaxError: redeclaration of let valueWillChange
+    <anonymous> debugger eval code:1
+debugger eval code:1:1
+    <anonymous> debugger eval code:1
+Empty string passed to getElementById(). 2 www.google.com
+Empty string passed to getElementById(). 2 www.google.com
+valueWillChange = 30;
+30
+const myString = 'hello world';
+undefined
+const myString2 = "hello world";
+undefined
+const myNumber = 30;
+undefined
+const myBoolean = false;
+undefined
+const myBoolean2 = true;
+undefined
+const myArray = [1, 2, 3, 4];
+undefined
+const secondArray = [ 'string 1', 'string 2'];
+undefined
+myArray[0];
+1
+const myObj = {
+  property1: 'some value',
+}
+undefined
+myObj.property1;
+"some value"
+myObj['property1'];
+"some value" 
